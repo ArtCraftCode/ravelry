93f01b7f58b4850abd072eea2e2a33ebfe2687ba
Liz Abinante <eabinante@gmail.com>
2013-12-22 15:58:57 -0600

    renamed needle to pattern_needle because this particular model is owned by pattern; a little redundant to have multiple needle objects, but it will make it easier later

---
+++ lib/ravelry/pattern_needle.rb
@@ -0,0 +1,15 @@
+module Ravelry
+
+  # The information used to create `Ravelry::PatternNeedle` currently comes from {Ravelry::Pattern} objects.
+  # 
+  # You should not need to create `PatterNeedle` objects manually; they are all created–and owned by–by a {Ravelry::Pattern}.
+  # 
+  # Note that there are other API endpoints for Needles; this might not be the one that you're looking for.
+  # 
+  # See {Ravelry::Pattern} for more information about `Pattern` objects.
+  #
+  class PatternNeedle
+    def initialize(needle)
+    end
+  end
+end
