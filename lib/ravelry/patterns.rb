fa232c62c0b283edb9b8a0934acb2bec700dad1a
Liz Abinante <eabinante@gmail.com>
2013-12-21 19:19:20 -0600

    api methods for patterns#show completed with 100% test coverage for gauge

--- lib/ravelry/patterns.rb
+++ lib/ravelry/patterns.rb
@@ -3,11 +3,12 @@ require 'curb'
 module Ravelry
 
   class Patterns
-    attr_reader :id, :pattern
+    attr_reader :pattern
+    attr_accessor :id
 
-    def initialize(id)
+    def initialize(id=nil)
       @id = id
-      @pattern = fetch_and_parse
+      @pattern = fetch_and_parse if @id
     end
 
     def fetch_and_parse
@@ -17,8 +18,66 @@ module Ravelry
       c.password = ENV['RAV_PERSONAL']
       c.perform
       result = JSON.parse(c.body_str, {symbolize_names: true})
-      return result[:pattern]
+      @pattern = result[:pattern]
     end
+
+    def comments_count
+      pattern[:comments_count]
+    end
+
+    def craft_name
+      pattern[:craft][:name]
+    end
+
+    def craft_permalink
+      pattern[:craft][:permalink]
+    end
+
+    def currency
+      pattern[:currency]
+    end
+
+    def currency_symbol
+      pattern[:currency_symbol]
+    end
+
+    def difficulty_average_float
+      pattern[:difficulty_average]
+    end
+
+    def difficulty_average_integer
+      difficulty_average_float.to_i
+    end
+
+    def difficulty_count
+      pattern[:difficulty_count]
+    end
+
+    def downloadable?
+      pattern[:downloadable]
+    end
+
+    def favorites_count
+      pattern[:favorites_count]
+    end
+
+    def free?
+      pattern[:free]
+    end
+
+    def gauge
+      pattern[:gauge]
+    end
+
+    # Could be super fancy and dynamtically create methods
+    # but then I wouldn't have much control over things, and the only way
+    # to really be sure about the results would be to test it.
+    # def set_attrs
+    #   pattern.each do |key, value|
+    #     self.class.send(:define_method, key) { value }
+    #   end
+    # end
+
   end
 
 end
--- spec/ravelry/patterns_spec.rb
+++ spec/ravelry/patterns_spec.rb
@@ -4,8 +4,14 @@ require_relative '../../lib/ravelry/patterns'
 describe Ravelry::Patterns do
 
   context '#initialize' do
-    it 'should return an error when not given a argument' do
-      expect { Ravelry::Patterns.new }.to raise_error(ArgumentError)
+    it 'should fetch @pattern if given @id' do
+      pattern = initialize_paid
+      expect(pattern.pattern).to be
+    end
+
+    it 'should not fetch pattern if given no @id' do
+      pattern = initialize_empty
+      expect(pattern.pattern).to be_nil
     end
   end
 
@@ -25,5 +31,83 @@ describe Ravelry::Patterns do
     it 'should return a hash' do
       expect(@api.pattern).to be_kind_of(Hash)
     end
+  end
+
+  context 'pattern attributes' do
+    before do
+      @api = initialize_empty
+      @api.stub(:pattern).and_return(paid_pattern_stub)
+      @data = paid_pattern_stub
+    end
+
+    it 'comments_count' do
+      expect(@api.comments_count).to eq(4)
+    end
+
+    it 'comments_count (Integer)' do
+      expect(@api.comments_count).to be_kind_of(Integer)
+    end
+
+    it 'craft_name' do
+      expect(@api.craft_name).to eq(@data[:craft][:name])
+    end 
+
+    it 'craft_permalink' do
+      expect(@api.craft_permalink).to eq(@data[:craft][:permalink])
+    end
+
+    it 'currency' do
+      expect(@api.currency).to eq(@data[:currency])
+    end
+
+    it 'currency_symbol' do
+      expect(@api.currency_symbol).to eq(@data[:currency_symbol])
+    end
+
+    it 'difficulty_average_float (Float)' do
+      expect(@api.difficulty_average_float).to be_kind_of(Float)
+    end
+
+    it 'difficulty_average_float' do
+      expect(@api.difficulty_average_float).to eq(@data[:difficulty_average])
+    end
+
+    it 'difficulty_average_integer (Integer)' do
+      expect(@api.difficulty_average_integer).to be_kind_of(Integer)
+    end
+
+    it 'difficulty_average_integer' do
+      expect(@api.difficulty_average_integer).to eq(@data[:difficulty_average].to_i)
+    end
+
+    it 'difficulty_count (Integer)' do
+      expect(@api.difficulty_count).to be_kind_of(Integer)
+    end
+
+    it 'difficulty_count' do
+      expect(@api.difficulty_count).to eq(@data[:difficulty_count])
+    end
+
+    it 'downloadable?' do
+      # testing data is true
+      expect(@api.downloadable?).to be_true
+    end
+
+    it 'favorites_count (Integer)' do
+      expect(@api.favorites_count).to be_kind_of(Integer)
+    end
+
+    it 'favorites_count' do
+      expect(@api.favorites_count).to eq(@data[:favorites_count])
+    end
+
+    it 'free?' do
+      # testing data is false
+      expect(@api.free?).to be_false
+    end
+
+    it 'gauge' do
+      expect(@api.gauge).to eq(@data[:gauge])
+    end 
   end
 end
