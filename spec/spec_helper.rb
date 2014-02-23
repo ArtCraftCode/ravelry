139f9bc7bae86e15ac2837aa2cd741b2e7239a58
Liz Abinante <eabinante@gmail.com>
2014-02-23 11:37:47 -0600

    deleted category helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,11 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'ravelry'
-require 'helpers'
-require 'category_helpers'
-require 'pattern_helpers'
-require 'pack_helpers'
-require 'yarn_helpers'
-require 'yarn_weight_helpers'
+require_relative 'helpers/helpers'
+require_relative 'helpers/pattern_helpers'
+require_relative 'helpers/pack_helpers'
+require_relative 'helpers/yarn_helpers'
+require_relative 'helpers/yarn_weight_helpers'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
@@ -15,7 +14,6 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
   config.include Helpers
-  config.include CategoryHelpers
   config.include PatternHelpers
   config.include PackHelpers
   config.include YarnHelpers
