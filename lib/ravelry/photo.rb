3fd448e558a910feec3e50c771fc5e4272fafacc
Liz Abinante <eabinante@gmail.com>
2014-02-23 12:10:43 -0600

    whitespace

--- lib/ravelry/photo.rb
+++ lib/ravelry/photo.rb
@@ -1,11 +1,11 @@
 module Ravelry
 
   # The information used to create `Ravelry::Photo` currently comes from {Ravelry::Pattern} objects.
-  # 
+  #
   # See {Ravelry::Pattern} for more information about `Pattern` objects.
-  # 
+  #
   # This class will be updated in future to perform GET, POST, PUT, and DESTROY requests.
-  # 
+  #
   class Photo < Data
   end
 end
