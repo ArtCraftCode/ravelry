3ae73ec64322d29033ed0cbac5658f2fa16fd865
Liz Abinante <eabinante@gmail.com>
2014-02-23 11:20:48 -0600

    whitespace

--- lib/ravelry/craft.rb
+++ lib/ravelry/craft.rb
@@ -1,11 +1,11 @@
 module Ravelry
 
   # The information used to create `Ravelry::Craft` currently comes from {Ravelry::Pattern} objects.
-  # 
+  #
   # You should not need to create `Craft` objects manually; they are all created–and owned by–by a {Ravelry::Pattern}.
-  # 
+  #
   # See {Ravelry::Pattern} for more information about `Pattern` objects.
-  # 
+  #
   class Craft
     def initialize(craft)
     end
