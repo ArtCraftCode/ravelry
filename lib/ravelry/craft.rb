8267d2e5246b73f957386149131ead45a800d3c2
Liz Abinante <eabinante@gmail.com>
2014-02-23 11:36:30 -0600

    documenting craft

--- lib/ravelry/craft.rb
+++ lib/ravelry/craft.rb
@@ -2,7 +2,7 @@ module Ravelry
 
   # The information used to create `Ravelry::Craft` currently comes from {Ravelry::Pattern} objects.
   #
-  # You should not need to create `Craft` objects manually; they are all created–and owned by–by a {Ravelry::Pattern}.
+  # You should not need to create `Craft` objects manually; they are all created–and owned–by a {Ravelry::Pattern}.
   #
   # See {Ravelry::Pattern} for more information about `Pattern` objects.
   #
