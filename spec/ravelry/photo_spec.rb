ccab41a32922eea087d96b41ddfd93340430d9ca
Liz Abinante <eabinante@gmail.com>
2014-03-27 14:49:15 -0500

    photo specs passing

--- spec/ravelry/photo_spec.rb
+++ spec/ravelry/photo_spec.rb
@@ -2,7 +2,7 @@ require_relative '../spec_helper'
 
 describe Ravelry::Photo do
   before do
-    @photo = Ravelry::Photo.new(photo)
+    @photo = Ravelry::Photo.new(nil, photo)
     @data = photo
   end
 
@@ -17,40 +17,41 @@ describe Ravelry::Photo do
     it 'get_sizes'
 
     it '#flickr_url' do
+      puts @photo
       puts @data
-      expect(@photo.flickr_url).to be(@data[:flickr_url])
+      expect(@photo.flickr_url).to eq(@data[:flickr_url])
     end
 
     it '#small_url' do
-      expect(@photo.small_url).to be(@data[:small_url])
+      expect(@photo.small_url).to eq(@data[:small_url])
     end
 
     it '#medium_url' do
-      expect(@photo.medium_url).to be(@data[:medium_url])
+      expect(@photo.medium_url).to eq(@data[:medium_url])
     end
 
     it '#square_url' do
-      expect(@photo.square_url).to be(@data[:square_url])
+      expect(@photo.square_url).to eq(@data[:square_url])
     end
 
     it '#thumbnail_url' do
-      expect(@photo.thumbnail_url).to be(@data[:thumbnail_url])
+      expect(@photo.thumbnail_url).to eq(@data[:thumbnail_url])
     end
 
     it '#shelved_url' do
-      expect(@photo.shelved_url).to be(@data[:shelved_url])
+      expect(@photo.shelved_url).to eq(@data[:shelved_url])
     end
 
     it '#sort_order' do
-      expect(@photo.sort_order).to be(@data[:sort_order])
+      expect(@photo.sort_order).to eq(@data[:sort_order])
     end
 
     it '#x_offset' do
-      expect(@photo.x_offset).to be(@data[:x_offset])
+      expect(@photo.x_offset).to eq(@data[:x_offset])
     end
 
     it '#y_offset' do
-      expect(@photo.y_offset).to be(@data[:y_offset])
+      expect(@photo.y_offset).to eq(@data[:y_offset])
     end
   end
 end
