b593f3e34428fe16bc40d2c5645a1d085d4c84af
Liz Abinante <eabinante@gmail.com>
2014-03-27 15:31:35 -0500

    fix doc spacing

--- lib/ravelry/pattern.rb
+++ lib/ravelry/pattern.rb
@@ -75,9 +75,10 @@ module Ravelry
 
     include Build
 
-    attr_reader :author, :yarns, :yarn_weights, :packs, :categories, :craft, :needles, :photos, :printings, :type
+    attr_reader :author, :categories, :craft, :needles, :packs, :photos, :printings, :type, :yarns, :yarn_weights
 
     # Handles GET API call and parses JSON response.
+    #
     def get
       c = Curl::Easy.new("https://api.ravelry.com/patterns/#{@id}.json")
       c.http_auth_types = :basic
@@ -90,107 +91,112 @@ module Ravelry
 
     # Creates all objects associated with your pattern; returns nothing; sets `attr_readers`.
     #
-    # Sets `attr_reader` for:
-    #
-    # * `author` - a {Ravelry::Author} object
-    # * `packs` - array of {Ravelry::Pack} objects
-    # * `yarns` - array of {Ravelry::Yarn} objects
-    # * `yarn_weights` - array of {Ravelry::YarnWeight} objects
-    #
     def build
       @author = Build.author(data)
       @categories = Build.categories(data)
       @craft = Build.craft(data)
       @needles = Build.needles(data)
       @packs = Build.packs(data)
-      @type = Build.pattern_type(data)
       @photos = Build.photos(data)
       @printings = Build.printings(data)
+      @type = Build.pattern_type(data)
       @yarns = Build.yarns(data)
       @yarn_weights = Build.yarn_weights(data)
+      nil
     end
 
     # Gets comments_count from existing `data`.
+    #
     def comments_count
       data[:comments_count]
     end
 
-    def craft
-      data[:craft]
-      # TODO: Create class
-    end
-
     # Gets currency from existing `data`.
+    #
     def currency
       data[:currency]
     end
 
     # Gets currency_symbol from existing `data`.
+    #
     def currency_symbol
       data[:currency_symbol]
     end
 
     # Returns the difficult average as a Float (this is how it is stored by Ravelry).
+    #
     def difficulty_average_float
       data[:difficulty_average]
     end
 
     # Returns the difficulty average rounded up or down to an Integer.
+    #
     def difficulty_average_integer
       difficulty_average_float.round(0)
     end
 
     # Gets difficulty_count (Integer) from existing `data`.
+    #
     def difficulty_count
       data[:difficulty_count]
     end
 
     # Returns true if the pattern can be downloaded.
+    #
     def downloadable?
       data[:downloadable]
     end
 
     # Gets favorites_count (Integer) from existing `data`.
+    #
     def favorites_count
       data[:favorites_count]
     end
 
     # Returns true if pattern is free (Boolean).
+    #
     def free?
       data[:free]
     end
 
     # Number of stitches per inch (or 4 inches) (Float).
+    #
     def gauge
       data[:gauge]
     end
 
     # Sentence description of sts and row gauge with stitch.
+    #
     def gauge_description
       data[:gauge_description]
     end
 
     # Either 1 or 4 (inches) (Integer).
+    #
     def gauge_divisor
       data[:gauge_divisor]
     end
 
     # Pattern for gauge listed.
+    #
     def gauge_pattern
       data[:gauge_pattern]
     end
 
     # Gets patter name from existing `data`.
+    #
     def name
       data[:name]
     end
 
     # Raw pattern notes. May be mixed Markdown and HTML. Generally only useful when presenting a pattern notes editor.
+    #
     def notes_raw
       data[:notes]
     end
 
     # Pattern notes rendered as HTML.
+    #
     def notes_html
       data[:notes_html]
     end
@@ -214,6 +220,7 @@ module Ravelry
     end
 
     # Helper that will tell you how many yarns you have in your pack.
+    #
     def pack_count
       data[:packs].length
     end
@@ -240,45 +247,43 @@ module Ravelry
 
     def pattern_needle_sizes_raw
       data[:pattern_needle_sizes]
-      # TODO: Create class
     end
 
     def pattern_type_raw
       data[:pattern_type]
-      # TODO: Create class
     end
 
     # Gets pdf_url from existing `data`.
+    #
     def pdf_url
       data[:pdf_url]
     end
 
     # Gets Ravelry permalink from existing `data`.
+    #
     def permalink
       data[:permalink]
     end
 
+    #
     def photos_raw
       data[:photos]
-      # TODO: Create class
     end
 
     # Gets price from existing `data` (Float).
+    #
     def price
       data[:price]
     end
 
-    def printings
-      data[:printings]
-      # TODO: Create class
-    end
-
     # Gets product_id from existing `data`.
+    #
     def product_id
       data[:product_id]
     end
 
     # Gets projects_count from existing `data` (Integer).
+    #
     def projects_count
       data[:projects_count]
     end
@@ -290,65 +295,69 @@ module Ravelry
     end
 
     # Gets number of queued projects from existing `data` (Integer).
+    #
     def queued_projects_count
       data[:queued_projects_count]
     end
 
     # Gets rating_average from existing `data` (Float).
+    #
     def rating_average
       data[:rating_average]
     end
 
     # Gets number of ratings from existing `data` (Integer).
+    #
     def rating_count
       data[:rating_count]
     end
 
     # Returns true if pattern is a Ravelry download (Boolean).
+    #
     def ravelry_download?
       data[:ravelry_download]
     end
 
     # Gets row gauge from existing `data` (Float).
+    #
     def row_gauge
       data[:row_gauge]
     end
 
     # Gets sizes available from existing `data`.
+    #
     def sizes_available
       data[:sizes_available]
     end
 
     # Gets url from existing `data`.
+    #
     def url
       data[:url]
     end
 
     # Gets yardage required from existing `data` (Integer).
+    #
     def yardage
       data[:yardage]
     end
 
-    # Gets nice sentence yardage description with range  from existing `data`.
+    # Gets nice sentence yardage description with range from existing `data`.
+    #
     def yardage_description
       data[:yardage_description]
     end
 
     # Gets max yards required from existing `data` (Integer).
+    #
     def yardage_max
       data[:yardage_max]
     end
 
-    # Gets primary yarn weight required from existing `data`.
-    def yarn_weight
-      data[:yarn_weight]
-    end
-
     # Gets primary yarn weight description from existing `data`.
+    #
     def yarn_weight_description
       data[:yarn_weight_description]
     end
-
   end
-
 end
