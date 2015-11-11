ec8786b20381efbf19f4cda3e5d5447c478ec7d4
Liz Abinante <eabinante@gmail.com>
2015-11-10 18:32:50 -0800

    add more docs

--- lib/ravelry/misc.rb
+++ lib/ravelry/misc.rb
@@ -1,5 +1,7 @@
 module Ravelry
 
+  # Handles API request for the `Misc` endpoints. Always returns an array of objects.
+  #
   module Misc
     extend self
 
@@ -12,6 +14,10 @@ module Ravelry
       result.data.map { |color_family| ColorFamily.new(color_family) }
     end
 
+    # Handles GET API call and parses JSON response for the `Misc#color_families` Ravelry endpoint.
+    #
+    # Returns an array of {Ravelry::ColorFamily} objects.
+    #
     def yarn_weights
       result = Utils::Request.get("yarn_weights.json", :yarn_weights)
       result.data.map { |yarn_weight| YarnWeight.new(yarn_weight) }
