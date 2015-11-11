4e02e10b7a3dccae46af410e2c55275914211ac6
Liz Abinante <eabinante@gmail.com>
2015-11-10 18:37:10 -0800

    change instance var request to regular var request

--- lib/ravelry/utils/request.rb
+++ lib/ravelry/utils/request.rb
@@ -5,7 +5,7 @@ module Ravelry
 
       def get(url, key)
         request = Typhoeus::Request.get("https://api.ravelry.com/#{url}", userpwd: "#{Ravelry.configuration.access_key}:#{Ravelry.configuration.personal_key}")
-        data = JSON.parse(@request.response_body, {symbolize_names: true})
+        data = JSON.parse(request.response_body, {symbolize_names: true})
         return data[key] if key
         return data
       end
