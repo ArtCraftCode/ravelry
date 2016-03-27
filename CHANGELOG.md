9224fd4f126b9841fb5ba9e6b50f1ad23aae4542
Liz Abinante <eabinante@gmail.com>
2016-03-27 15:21:15 -0700

    :tophat: remove require dir :bug:

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,6 @@
 ## 0.1.0
 
 - Return `self` on both `Ravelry::Pattern.get` and `Ravelry::Pattern.permalink_get` (previously returned `data`)
-- Auto-magically require all `lib/ravelry/*.rb` and `lib/ravelry/utils/*.rb` files
 - Add `callback_url` configuration option for OAuth
 - Create `ConfigurationError` class for configuration errors
 
--- lib/ravelry.rb
+++ lib/ravelry.rb
@@ -1,10 +1,32 @@
 require 'json'
 require 'typhoeus'
 
-DIRECTORIES = [ "#{File.dirname(__FILE__)}/ravelry/utils/*.rb",
-                "#{File.dirname(__FILE__)}/ravelry/*.rb" ]
+require 'ravelry/configuration'
+require 'ravelry/configuration_error'
 
-DIRECTORIES.map { |dir| Dir[dir].each { |file| require file } }
+require 'ravelry/utils/build'
+require 'ravelry/utils/request'
+require 'ravelry/utils/utilities'
+
+require 'ravelry/author'
+require 'ravelry/category'
+require 'ravelry/color_family'
+require 'ravelry/comment'
+require 'ravelry/craft'
+require 'ravelry/data'
+require 'ravelry/misc'
+require 'ravelry/needle'
+require 'ravelry/needles'
+require 'ravelry/pack'
+require 'ravelry/pattern'
+require 'ravelry/pattern_type'
+require 'ravelry/photo'
+require 'ravelry/printing'
+require 'ravelry/social_site'
+require 'ravelry/user'
+require 'ravelry/user_site'
+require 'ravelry/yarn'
+require 'ravelry/yarn_weight'
 
 # Parent module for all classes.
 # 
